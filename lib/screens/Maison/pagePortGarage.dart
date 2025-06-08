71520dcf63da0803a7a970b45d85df4ca17c7d3b
GHALIABELHAJSGHAIER <ghalia.bhs20020130@gmail.com>
2025-06-08 18:04:35 +0200

    empreinte

--- lib/screens/Maison/pagePortGarage.dart
+++ lib/screens/Maison/pagePortGarage.dart
@@ -1,7 +1,218 @@
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'package:intl/intl.dart';
+// import 'package:clone_spotify_mars/controllers/Maison/portGarage_controller.dart';
+// //import 'package:clone_spotify_mars/models/Maison/portGarage_model.dart';
+
+// class PortGaragePage extends StatefulWidget {
+//   final String maisonId;
+
+//   const PortGaragePage({Key? key, required this.maisonId}) : super(key: key);
+
+//   @override
+//   _PortGaragePageState createState() => _PortGaragePageState();
+// }
+
+// class _PortGaragePageState extends State<PortGaragePage> {
+//   final PortgarageController portGarageController = Get.put(
+//     PortgarageController(),
+//   );
+//   String? selectedGarageId;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     WidgetsBinding.instance.addPostFrameCallback((_) {
+//       portGarageController.getPortGarageByIdMaison(widget.maisonId);
+//     });
+//   }
+
+//   void _showHistoryBottomSheet(BuildContext context, String garageId) {
+//     final controller = Get.find<PortgarageController>();
+//     controller.getHistoriqueByGarageId(garageId);
+//     showModalBottomSheet(
+//       context: context,
+//       builder: (context) {
+//         return Obx(() {
+//           final historique =
+//               controller.historique
+//                   .where((h) => h.garageId == garageId)
+//                   .toList()
+//                 ..sort((a, b) => b.date.compareTo(a.date));
+
+//           return AlertDialog(
+//             title: const Text("Historique"),
+//             content: Container(
+//               width: double.maxFinite,
+//               child:
+//                   historique.isEmpty
+//                       ? const Text("Aucun historique disponible")
+//                       : ListView.builder(
+//                         shrinkWrap: true,
+//                         itemCount: historique.length,
+//                         itemBuilder: (context, index) {
+//                           final item = historique[index];
+//                           return ListTile(
+//                             leading: Icon(
+//                               item.etat ? Icons.lock_open : Icons.lock,
+//                               color: item.etat ? Colors.green : Colors.red,
+//                             ),
+//                             title: Text(item.etat ? "Ouvert" : "Fermé"),
+//                             subtitle: Text(
+//                               DateFormat('dd/MM HH:mm').format(item.date),
+//                             ),
+//                             trailing: IconButton(
+//                               icon: Icon(Icons.delete, color: Colors.red),
+//                               onPressed: () async {
+//                                 final confirm = await showDialog<bool>(
+//                                   context: context,
+//                                   builder:
+//                                       (_) => AlertDialog(
+//                                         title: const Text("Confirmation"),
+//                                         content: const Text(
+//                                           "Voulez-vous vraiment supprimer cet historique ?",
+//                                         ),
+//                                         actions: [
+//                                           TextButton(
+//                                             onPressed:
+//                                                 () => Navigator.pop(
+//                                                   context,
+//                                                   false,
+//                                                 ),
+//                                             child: const Text("Annuler"),
+//                                           ),
+//                                           TextButton(
+//                                             onPressed:
+//                                                 () => Navigator.pop(
+//                                                   context,
+//                                                   true,
+//                                                 ),
+//                                             child: const Text("Supprimer"),
+//                                           ),
+//                                         ],
+//                                       ),
+//                                 );
+
+//                                 if (confirm == true) {
+//                                   controller.deleteHistoriqueById(item.id);
+//                                 }
+//                               },
+//                             ),
+//                           );
+//                         },
+//                       ),
+//             ),
+//             actions: [
+//               TextButton(
+//                 onPressed: () => Navigator.pop(context),
+//                 child: const Text("Fermer"),
+//               ),
+//             ],
+//           );
+//         });
+//       },
+//     );
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text("Mes portes de garage"),
+//         centerTitle: true,
+//         actions: [
+//           IconButton(
+//             icon: const Icon(Icons.refresh),
+//             onPressed: () {
+//               portGarageController.getPortGarageByIdMaison(widget.maisonId);
+//             },
+//           ),
+//         ],
+//       ),
+//       body: Obx(() {
+//         if (portGarageController.garages.isEmpty) {
+//           return const Center(child: CircularProgressIndicator());
+//         }
+
+//         return ListView.builder(
+//           padding: const EdgeInsets.all(16),
+//           itemCount: portGarageController.garages.length,
+//           itemBuilder: (context, index) {
+//             final garage = portGarageController.garages[index];
+//             return Card(
+//               elevation: 4,
+//               margin: const EdgeInsets.only(bottom: 16),
+//               child: Padding(
+//                 padding: const EdgeInsets.all(12),
+//                 child: Column(
+//                   crossAxisAlignment: CrossAxisAlignment.start,
+//                   children: [
+//                     Row(
+//                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                       children: [
+//                         Row(
+//                           children: [
+//                             Icon(
+//                               Icons.garage,
+//                               size: 30,
+//                               color:
+//                                   garage.portGarage ? Colors.green : Colors.red,
+//                             ),
+//                             const SizedBox(width: 10),
+//                             Text(
+//                               "Porte de garage ",
+//                               style: const TextStyle(
+//                                 fontSize: 18,
+//                                 fontWeight: FontWeight.bold,
+//                               ),
+//                             ),
+//                           ],
+//                         ),
+//                         Switch(
+//                           value: garage.portGarage,
+//                           onChanged: (value) {
+//                             portGarageController.updatePortGarageByIdGarage(
+//                               garage.id,
+//                               value,
+//                             );
+//                           },
+//                           activeColor: Colors.green,
+//                         ),
+//                       ],
+//                     ),
+//                     const SizedBox(height: 8),
+//                     Text(
+//                       "État: ${garage.portGarage ? "Ouvert" : "Fermé"}",
+//                       style: TextStyle(
+//                         color: garage.portGarage ? Colors.green : Colors.red,
+//                       ),
+//                     ),
+//                     const SizedBox(height: 8),
+//                     Align(
+//                       alignment: Alignment.centerRight,
+//                       child: TextButton(
+//                         onPressed:
+//                             () => _showHistoryBottomSheet(context, garage.id),
+//                         child: const Text("Voir l'historique"),
+//                       ),
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//             );
+//           },
+//         );
+//       }),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 import 'package:clone_spotify_mars/controllers/Maison/portGarage_controller.dart';
+import 'package:local_auth/local_auth.dart';
+
 //import 'package:clone_spotify_mars/models/Maison/portGarage_model.dart';
 
 class PortGaragePage extends StatefulWidget {
@@ -14,6 +225,8 @@ class PortGaragePage extends StatefulWidget {
 }
 
 class _PortGaragePageState extends State<PortGaragePage> {
+  final LocalAuthentication auth = LocalAuthentication();
+
   final PortgarageController portGarageController = Get.put(
     PortgarageController(),
   );
@@ -25,6 +238,25 @@ class _PortGaragePageState extends State<PortGaragePage> {
     WidgetsBinding.instance.addPostFrameCallback((_) {
       portGarageController.getPortGarageByIdMaison(widget.maisonId);
     });
+  }
+
+  Future<bool> authenticate() async {
+    final isAvailable = await auth.canCheckBiometrics;
+
+    if (isAvailable) {
+      final didAuthenticate = await auth.authenticate(
+        localizedReason: 'Veuillez scanner votre empreinte pour continuer',
+        options: const AuthenticationOptions(
+          biometricOnly: true,
+          stickyAuth: true,
+        ),
+      );
+
+      return didAuthenticate;
+    } else {
+      Get.snackbar("Erreur", "Biométrie non disponible sur ce téléphone.");
+      return false;
+    }
   }
 
   void _showHistoryBottomSheet(BuildContext context, String garageId) {
@@ -168,13 +400,31 @@ class _PortGaragePageState extends State<PortGaragePage> {
                             ),
                           ],
                         ),
+                        // Switch(
+                        //   value: garage.portGarage,
+                        //   onChanged: (value) {
+                        //     portGarageController.updatePortGarageByIdGarage(
+                        //       garage.id,
+                        //       value,
+                        //     );
+                        //   },
+                        //   activeColor: Colors.green,
+                        // ),
                         Switch(
                           value: garage.portGarage,
-                          onChanged: (value) {
-                            portGarageController.updatePortGarageByIdGarage(
-                              garage.id,
-                              value,
-                            );
+                          onChanged: (value) async {
+                            bool isAuth = await authenticate();
+                            if (isAuth) {
+                              portGarageController.updatePortGarageByIdGarage(
+                                garage.id,
+                                value,
+                              );
+                            } else {
+                              Get.snackbar(
+                                "Accès refusé",
+                                "Authentification biométrique échouée.",
+                              );
+                            }
                           },
                           activeColor: Colors.green,
                         ),
--- lib/screens/signin_page.dart
+++ lib/screens/signin_page.dart
@@ -290,3 +290,319 @@ class _SigninPageState extends State<SigninPage> {
     );
   }
 }
+
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:form_field_validator/form_field_validator.dart';
+// import 'package:get/get.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
+// import '../controllers/auth_controller.dart';
+// import '../models/user_model.dart';
+// import '../screens/home_page.dart';
+// import '../screens/signup_page.dart';
+// import 'package:local_auth/local_auth.dart';
+
+// class SigninPage extends StatefulWidget {
+//   const SigninPage({super.key});
+
+//   @override
+//   State<SigninPage> createState() => _SigninPageState();
+// }
+
+// class _SigninPageState extends State<SigninPage> {
+//   final LocalAuthentication auth = LocalAuthentication();
+
+//   late TextEditingController _emailController;
+//   late TextEditingController _passwordController;
+//   late GlobalKey<FormState> _formkey;
+//   bool _isObscure = true;
+
+//   final controller = Get.put(AuthController());
+//   SharedPreferences? prefs;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _emailController = TextEditingController();
+//     _passwordController = TextEditingController();
+//     _formkey = GlobalKey<FormState>();
+//     initSharedPref();
+//   }
+
+//   Future<void> initSharedPref() async {
+//     prefs = await SharedPreferences.getInstance();
+//   }
+
+//   @override
+//   void dispose() {
+//     _emailController.dispose();
+//     _passwordController.dispose();
+//     controller.dispose();
+//     super.dispose();
+//   }
+
+//   Future<void> authenticate() async {
+//     try {
+//       final isAvailable = await auth.canCheckBiometrics;
+//       final isDeviceSupported = await auth.isDeviceSupported();
+
+//       if (isAvailable && isDeviceSupported) {
+//         final didAuthenticate = await auth.authenticate(
+//           localizedReason:
+//               'Veuillez scanner votre empreinte pour vous connecter',
+//           options: const AuthenticationOptions(
+//             biometricOnly: true,
+//             stickyAuth: true,
+//           ),
+//         );
+
+//         if (didAuthenticate) {
+//           final token = prefs?.getString('token');
+//           if (token != null && token.isNotEmpty) {
+//             Navigator.pushAndRemoveUntil(
+//               context,
+//               MaterialPageRoute(builder: (context) => HomePage(token: token)),
+//               (route) => false,
+//             );
+//           } else {
+//             ScaffoldMessenger.of(context).showSnackBar(
+//               SnackBar(
+//                 content: Text(
+//                   "Aucun token enregistré. Veuillez vous connecter manuellement.",
+//                 ),
+//               ),
+//             );
+//           }
+//         } else {
+//           ScaffoldMessenger.of(context).showSnackBar(
+//             SnackBar(content: Text("Échec de l'authentification biométrique")),
+//           );
+//         }
+//       } else {
+//         ScaffoldMessenger.of(context).showSnackBar(
+//           SnackBar(content: Text("Biométrie non disponible sur cet appareil")),
+//         );
+//       }
+//     } catch (e) {
+//       ScaffoldMessenger.of(
+//         context,
+//       ).showSnackBar(SnackBar(content: Text("Erreur : ${e.toString()}")));
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return SafeArea(
+//       child: Scaffold(
+//         appBar: AppBar(
+//           foregroundColor: Colors.black,
+//           backgroundColor: Colors.white,
+//           title: Text(
+//             "Se connecter",
+//             style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+//           ),
+//           centerTitle: true,
+//         ),
+//         body: Container(
+//           width: double.infinity,
+//           height: double.infinity,
+//           decoration: BoxDecoration(
+//             image: DecorationImage(
+//               image: AssetImage("assets/maison.png"),
+//               fit: BoxFit.cover,
+//             ),
+//           ),
+//           child: SingleChildScrollView(
+//             padding: EdgeInsets.all(10),
+//             child: Form(
+//               key: _formkey,
+//               child: Column(
+//                 crossAxisAlignment: CrossAxisAlignment.start,
+//                 children: [
+//                   Center(child: Image.asset("assets/logo_text.png")),
+//                   Center(
+//                     child: Text(
+//                       "Créez un compte une fois et connectez-vous à tous nos services",
+//                       textAlign: TextAlign.center,
+//                       style: TextStyle(
+//                         color: Colors.white,
+//                         fontWeight: FontWeight.w400,
+//                         fontSize: 24,
+//                       ),
+//                     ),
+//                   ),
+//                   SizedBox(height: 10),
+//                   Text(
+//                     "Email",
+//                     style: TextStyle(
+//                       color: Colors.white,
+//                       fontSize: 20,
+//                       fontWeight: FontWeight.bold,
+//                     ),
+//                   ),
+//                   SizedBox(height: 10),
+//                   TextFormField(
+//                     controller: _emailController,
+//                     decoration: InputDecoration(
+//                       fillColor: Color.fromARGB(255, 76, 76, 76),
+//                       filled: true,
+//                       border: OutlineInputBorder(
+//                         borderRadius: BorderRadius.circular(20),
+//                       ),
+//                       labelText: "Email",
+//                       labelStyle: TextStyle(color: Colors.white, fontSize: 20),
+//                       prefixIcon: Icon(
+//                         Icons.email_outlined,
+//                         color: Colors.white,
+//                       ),
+//                     ),
+//                     style: TextStyle(color: Colors.white, fontSize: 20),
+//                     validator: MultiValidator([
+//                       EmailValidator(errorText: 'enter a valid email address'),
+//                       RequiredValidator(errorText: "email is required"),
+//                     ]),
+//                   ),
+//                   SizedBox(height: 10),
+//                   Text(
+//                     "Password",
+//                     style: TextStyle(
+//                       color: Colors.white,
+//                       fontSize: 20,
+//                       fontWeight: FontWeight.bold,
+//                     ),
+//                   ),
+//                   SizedBox(height: 10),
+//                   TextFormField(
+//                     controller: _passwordController,
+//                     obscureText: _isObscure,
+//                     decoration: InputDecoration(
+//                       fillColor: Color.fromARGB(255, 76, 76, 76),
+//                       filled: true,
+//                       border: OutlineInputBorder(
+//                         borderRadius: BorderRadius.circular(20),
+//                       ),
+//                       labelText: "password",
+//                       labelStyle: TextStyle(color: Colors.white, fontSize: 20),
+//                       prefixIcon: Icon(Icons.lock, color: Colors.white),
+//                       suffixIcon: IconButton(
+//                         onPressed: () {
+//                           setState(() {
+//                             _isObscure = !_isObscure;
+//                           });
+//                         },
+//                         icon: Icon(
+//                           _isObscure
+//                               ? CupertinoIcons.eye_slash
+//                               : CupertinoIcons.eye,
+//                           color: Colors.white,
+//                         ),
+//                       ),
+//                     ),
+//                     style: TextStyle(color: Colors.white, fontSize: 20),
+//                     validator: MultiValidator([
+//                       RequiredValidator(errorText: 'password is required'),
+//                       MinLengthValidator(8, errorText: 'minimum 8 caractères'),
+//                       MaxLengthValidator(
+//                         15,
+//                         errorText: 'maximum 15 caractères',
+//                       ),
+//                     ]),
+//                   ),
+//                   SizedBox(height: 20),
+//                   Column(
+//                     crossAxisAlignment: CrossAxisAlignment.center,
+//                     children: [
+//                       CupertinoButton(
+//                         color: Color.fromARGB(255, 156, 187, 201),
+//                         borderRadius: BorderRadius.circular(20),
+//                         onPressed: () async {
+//                           if (_formkey.currentState!.validate()) {
+//                             final user = UserModel(
+//                               email: _emailController.text,
+//                               password: _passwordController.text,
+//                             );
+//                             var result = await controller.signinController(
+//                               user,
+//                             );
+//                             if (result['status'] == true) {
+//                               var mytoken = result['token'];
+//                               prefs?.setString('token', mytoken);
+//                               Navigator.pushAndRemoveUntil(
+//                                 context,
+//                                 MaterialPageRoute(
+//                                   builder:
+//                                       (context) => HomePage(token: mytoken),
+//                                 ),
+//                                 (Route<dynamic> route) => false,
+//                               );
+//                             } else {
+//                               ScaffoldMessenger.of(context).showSnackBar(
+//                                 SnackBar(content: Text(result['error'])),
+//                               );
+//                             }
+//                           }
+//                         },
+//                         child: Text(
+//                           "Se connecter",
+//                           style: TextStyle(
+//                             color: Colors.white,
+//                             fontWeight: FontWeight.bold,
+//                             fontSize: 20,
+//                           ),
+//                         ),
+//                       ),
+//                       SizedBox(height: 20),
+//                       CupertinoButton(
+//                         borderRadius: BorderRadius.circular(20),
+//                         onPressed: () async {
+//                           if (prefs == null) {
+//                             await initSharedPref();
+//                           }
+//                           await authenticate();
+//                         },
+//                         child: Row(
+//                           mainAxisAlignment: MainAxisAlignment.center,
+//                           children: const [
+//                             Icon(Icons.fingerprint, color: Colors.white),
+//                           ],
+//                         ),
+//                       ),
+//                       SizedBox(height: 10),
+//                       TextButton(
+//                         onPressed: () {
+//                           Navigator.push(
+//                             context,
+//                             MaterialPageRoute(
+//                               builder: (context) => const SignupPage(),
+//                             ),
+//                           );
+//                         },
+//                         child: RichText(
+//                           textAlign: TextAlign.center,
+//                           text: TextSpan(
+//                             style: Theme.of(context).textTheme.headlineSmall!
+//                                 .copyWith(color: Colors.white),
+//                             children: const [
+//                               TextSpan(text: "Vous n'avez pas de compte ? "),
+//                               TextSpan(
+//                                 text: "S'inscrire",
+//                                 style: TextStyle(
+//                                   color: Color.fromARGB(255, 156, 187, 201),
+//                                   fontWeight: FontWeight.bold,
+//                                 ),
+//                               ),
+//                             ],
+//                           ),
+//                         ),
+//                       ),
+//                     ],
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
